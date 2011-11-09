78496adfb24de4ba487b6f5630073592ea459721
Stevan Litobac <stevan@litobac.com>
2011-11-09 16:48:03 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ttlunch2012::Application.config.secret_token = 'bc1d9483c8569f3734d214f6cce4a68df27b3ef16ac4fae9697be0369e453aaeaf0d704998fad63b3eb4db6bd2ed28556d98e315f29779ed74898e1cad12238e'
