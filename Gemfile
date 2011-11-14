bde05a06799feea6c23b743edcd4fc50786e1dca
Stevan Litobac <stevan@litobac.com>
2011-11-14 19:50:59 +0000

    backgrounds, brochure form image

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.1.1'
+# gem 'mysql', '~> 2.8.1'
 gem 'mysql2'
 
 gem 'simple_form'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,4 +11,3 @@
 //= require bootstrap-dropdown
 //= require bootstrap-tabs
 //= require jquery.nivo.slider.pack
-//= require supersized.core.3.2.0.min
