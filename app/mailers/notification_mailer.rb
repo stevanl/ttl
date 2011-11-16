cf530ef9af927d9ccc16d329c4a54bf93554765f
Stevan Litobac <stevan@litobac.com>
2011-11-16 15:05:48 +0000

    Other emails following suite. Please function as required.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,18 @@ class NotificationMailer < ActionMailer::Base
   default from: "stevan@independentfx.com"
   def brochure_email(person)
     @person = person
-    mail(:to => 'stevan@mycurrencytransfer.com', :subject => "New Brochure Order Form")
+    mail(:to => 'stevan@mycurrencytransfer.com', :subject => "[TTL]: New Brochure Order Form")
+  end
+  def rsvp_email(person)
+    @person = person
+    mail(:to => 'stevan@mycurrencytransfer.com', :subject => "[TTL]: New RSVP Form")
+  end
+  def nomination_email(person)
+    @person = person
+    mail(:to => 'stevan@mycurrencytransfer.com', :subject => "[TTL]: New Nomination Submitted")
+  end
+  def table_booking_email(person)
+    @person = person
+    mail(:to => 'stevan@mycurrencytransfer.com', :subject => "[TTL]: New Table Booking")
   end
 end
