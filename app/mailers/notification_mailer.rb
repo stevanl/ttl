cb7f53991164b0b2041c1e70d84171e9f7e89480
Stevan Litobac <stevan@litobac.com>
2011-11-21 15:07:30 +0000

    Try the name thing for emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "awards@traine-traide.org.uk"
+  default from: "TrainE-TraidE Awards <awards@traine-traide.org.uk>"
   def brochure_email(person)
     @person = person
     mail(:to => ['mark@jam-events.com', 'debbie@traine-traide.org.uk', 'shraga@traine-traide.org.uk', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "TTL: New Brochure Order Form")
