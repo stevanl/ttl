99b9df6112299f79c74b6ef9c49ef9390d33cab4
Stevan Litobac <stevan@litobac.com>
2011-12-12 12:53:30 +0000

    3 Change Update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,4 @@
 //= require bootstrap-tabs
 //= require jquery.nivo.slider.pack
 //= require supersized.core.3.2.0.min
+//= require jquery.validate.min
--- app/models/table_booking.rb
+++ app/models/table_booking.rb
@@ -1,2 +1,3 @@
 class TableBooking < ActiveRecord::Base
+  validates_presence_of :name, :company, :email
 end
